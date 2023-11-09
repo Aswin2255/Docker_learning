f56d55305de4a6f37d1a29bb673ffd534907d65a
Aswin2255 <mynameisaswin321@gmail.com>
2023-11-09 18:22:00 +0530

    docker-compose.yaml added

--- sampleapi1/index.js
+++ sampleapi1/index.js
@@ -1,39 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/mydb',{}).then((data)=>{
-    console.log('connecte8')
-}).catch((er)=>{
-    console.log(er)
-})
+const express = require("express");
+const app = express();
+const port = 3001;
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://mongodbi_db:27017/mydb", {})
+  .then((data) => {
+    console.log("connecte8");
+  })
+  .catch((er) => {
+    console.log(er);
+  });
 const newentry = new mongoose.Schema({
-    text:{
-        type : String
-    },
-    date:{
-        type:String,
-        default:Date.now()
-    }
-})
-const entry = mongoose.model('entryofdb',newentry)
-app.get('/h',(req,res)=>{
-    res.sendFile(__dirname + '/index.html')
-})
-app.get('/',async(req,res)=>{
+  text: {
+    type: String,
+  },
+  date: {
+    type: String,
+    default: Date.now(),
+  },
+});
+const entry = mongoose.model("entryofdb", newentry);
+app.get("/h", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/", async (req, res) => {
   try {
     const newdata = new entry({
-        text:"hellow"
-    })
-    await newdata.save()
-    res.send('entry is created')
-    
+      text: "hellow",
+    });
+    await newdata.save();
+    res.send("entry is created");
   } catch (error) {
-    console.log(error)
-    console.log('er')
-    
+    console.log(error);
+    console.log("er");
   }
-})
-app.listen(port,()=>{
-    console.log(`api running @ ${port}`)
-})
+});
+app.listen(port, () => {
+  console.log(`api running @ ${port}`);
+});
